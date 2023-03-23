c4ba122596d7f4610da5c16f2462e256d8556dba
Samet Ersoy <sametersoy@yandex.com>
2023-03-23 17:36:48 +0300

    Add project files.

---
+++ BackendHeyTaxi/Controllers/LocationController.cs
@@ -0,0 +1,32 @@
+using MarketBackend;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace BackendHeyTaxi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocationController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<LocationController> _logger;
+
+        public LocationController(ILogger<LocationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetLocation")]
+        public async Task<locations> GetLocation()
+        {
+            DataDbContext db = new DataDbContext();
+            var locations= await (from q in db.locations orderby q.id descending select q).FirstOrDefaultAsync();
+            return locations;
+        }
+    }
+}
---
+++ BackendHeyTaxi/DataDbContext.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketBackend
+{
+    public class DataDbContext : DbContext
+    {
+
+        public DbSet<locations> locations { get; set; }
+        public DbSet<prices> prices { get; set; }
+        public DbSet<stocks> stocks { get; set; }
+        public DbSet<orders> orders { get; set; }
+
+        public DbSet<companys> companys { get; set; }
+        public DbSet<product_details> product_details { get; set; }
+        public DbSet<order_details> order_details { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                => optionsBuilder.UseNpgsql("Host=192.168.1.61;Username=postgres;Password=23054q;Database=TaxiDB");
+    }
+
+    public class locations
+    {
+        public int id { get; set; }
+        public string? longitude { get; set; }
+        public string? latitude { get; set; }
+        public string? accuracy { get; set; }
+        public string ? heading { get; set; }
+        public string? altitude { get; set; }
+        public string? altitudeaccuracy { get; set; }
+        public string? floor { get; set; }
+        public string? timestamp { get; set; }
+        public string? frommockprovider { get; set; }
+        public DateTime? created_date { get; set; }
+        public string? created_by { get; set; }
+        public string? speedaccuracy { get; set; }
+        public string? speed { get; set; }
+
+        
+
+    }
+
+    public class prices
+    {
+        public int id { get; set; }
+        public int product_id { get; set; }
+        public float price { get; set; }
+        public string? updated_by { get; set; }
+        public DateTime? updated_date { get; set; }
+        public string? created_by { get; set; }
+        public DateTime? created_date { get; set; }
+        public int ?kdv { get; set; }
+
+    }
+
+    public class stocks
+    {
+        public int id { get; set; }
+        public int piece { get; set; }
+        public int product_id { get; set; }
+        public DateTime? created_date { get; set; }
+        public DateTime? updated_date { get; set; }
+        public string ?created_by { get; set; }
+        public string ?updated_by { get; set; }
+
+    }
+
+    public class orders
+    {
+        public int id { get; set; }
+        public float price { get; set; }
+        public int piece { get; set; }
+        public DateTime? updated_date { get; set; }
+        public DateTime? created_date { get; set; }
+        public string? created_by { get; set; }
+        public string? updated_by { get; set; }
+        public int? company_id { get; set; }
+    }
+    public class order_details
+    {
+        public int id { get; set; }
+        public int order_id { get; set; }
+        public int product_id { get; set; }
+        public float price { get; set; }
+        public int? company_id { get; set; }
+        public DateTime? updated_date { get; set; }
+        public DateTime? created_date { get; set; }
+        public string? created_by { get; set; }
+        public string? updated_by { get; set; }
+
+    }
+    public class companys
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+
+        public string adress { get; set; }
+
+        public int product_id { get; set; }
+
+        public int order_id { get; set; }
+        public DateTime created_date { get; set; }
+        public DateTime ?updated_date { get; set; }
+        public string created_by { get; set; }
+        public string ?updated_by { get; set; }
+    }
+
+    public class product_details
+    {
+        public int id { get; set; }
+
+        public DateTime? expiration_date { get; set; }
+
+        public string ?supplier { get; set; }
+
+        public DateTime ?production_date { get; set; }
+
+        public string ?picture { get; set; }
+
+        public int ?product_id { get; set; }
+
+        public DateTime ?created_date { get; set; }
+        public DateTime? updated_date { get; set; }
+        public string ?created_by { get; set; }
+        public string? updated_by { get; set; }
+    }
+}
